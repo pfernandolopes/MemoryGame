4332bbd11eee6dc339afd42311baf5da24466e14
pfernandolopes <53016713+pfernandolopes@users.noreply.github.com>
2026-05-20 17:42:13 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,21 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = "bluey-memory-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('bluey-memory-game-v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/styles.css',
-        '/icons/bluey-icon-192.png',
-        '/icons/bluey-icon-512.png'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
-  console.log('Service Worker: Installed');
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker: Activated');
 });
